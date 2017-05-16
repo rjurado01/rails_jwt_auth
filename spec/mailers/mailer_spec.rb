0adb4f2f4f689e45d5633d464ca20d3244f28b1b
rjurado <rjurado@openmailbox.org>
2017-05-16 08:16:19 +0200

    Added missing tests of mailer.

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -90,4 +90,43 @@ RSpec.describe RailsJwtAuth::Mailer, type: :mailer do
       end
     end
   end
+
+  describe 'set_password_instructions' do
+    let(:user) do
+      FactoryGirl.create(:active_record_user,
+        reset_password_token: 'abcd', reset_password_sent_at: Time.now)
+    end
+
+    let(:mail) { described_class.set_password_instructions(user).deliver_now }
+
+    it 'sends email with correct info' do
+      expect { mail }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      expect(mail.subject).to eq(I18n.t('rails_jwt_auth.mailer.set_password_instructions.subject'))
+      expect(mail.to).to include(user.email)
+      expect(mail.from).to include(RailsJwtAuth.mailer_sender)
+      expect(mail.body).to include(password_url(reset_password_token: user.reset_password_token))
+    end
+
+    context 'when set_password_url opton is defined' do
+      before do
+        RailsJwtAuth.set_password_url = 'http://my-url.com'
+      end
+
+      it 'uses this to generate reset_password url' do
+        url = "#{RailsJwtAuth.set_password_url}?reset_password_token=#{user.reset_password_token}"
+        expect(mail.body).to include(url)
+      end
+    end
+
+    context 'when set_password_url opton is defined with hash url' do
+      before do
+        RailsJwtAuth.set_password_url = 'http://www.host.com/#/url?param=value'
+      end
+
+      it 'uses this to generate confirmation url' do
+        url = "#{RailsJwtAuth.set_password_url}&reset_password_token=#{user.reset_password_token}"
+        expect(mail.body).to include(url)
+      end
+    end
+  end
 end
