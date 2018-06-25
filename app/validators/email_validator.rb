20adb38de403614d64ba8f95c2a39a9784a55a1d
Juan Antonio Galisteo Prieto <jgalisteo@nosolosoftware.es>
2018-06-25 08:27:12 +0200

    Errors details for authenticatable module.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,7 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ RailsJwtAuth.email_regex
-      record.errors[attribute] << (options[:message] || I18n.t('rails_jwt_auth.errors.email.invalid'))
+      record.errors.add(attribute, (options[:message] || 'invalid'))
     end
   end
 end
