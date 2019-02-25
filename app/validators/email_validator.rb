39d3310744ff1f7225888de99a06a5d48b9c1d33
rjurado01 <rjurado01@gmail.com>
2019-02-25 14:39:50 +0100

    Removed residual email validator.

--- app/validators/email_validator.rb
+++
@@ -1,7 +0,0 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ RailsJwtAuth.email_regex
-      record.errors.add(attribute, (options[:message] || 'invalid'))
-    end
-  end
-end
