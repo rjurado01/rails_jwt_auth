8c264001f1dd84f1b3fd3ec823d16f57f8f0a12c
rjurado01 <rjurado01@gmail.com>
2019-03-19 13:47:18 +0100

    Restore sign_out spec helper.

--- lib/rails_jwt_auth/spec_helpers.rb
+++ lib/rails_jwt_auth/spec_helpers.rb
@@ -1,15 +1,19 @@
 module RailsJwtAuth
   module SpecHelpers
-    def sign_out
-      warn '[DEPRECATION] `sign_out` is deprecated and not needed.  Please remove it.'
-    end
-
     def sign_in(user)
       allow_any_instance_of(RailsJwtAuth::AuthenticableHelper)
         .to receive(:authenticate!).and_return(true)
 
       allow_any_instance_of(RailsJwtAuth::AuthenticableHelper)
         .to receive(:current_user).and_return(user)
+    end
+
+    def sign_out
+      allow_any_instance_of(RailsJwtAuth::AuthenticableHelper)
+        .to receive(:authenticate!).and_call_original
+
+      allow_any_instance_of(RailsJwtAuth::AuthenticableHelper)
+        .to receive(:current_user).and_call_original
     end
   end
 end
