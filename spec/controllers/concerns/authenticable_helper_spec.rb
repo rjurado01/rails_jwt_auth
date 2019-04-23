cd2904440542147075ada74a3bd59e8088e0a917
rjurado01 <rjurado01@gmail.com>
2019-04-23 20:14:07 +0200

    Added authenticate helper.

--- spec/controllers/concerns/authenticable_helper_spec.rb
+++ spec/controllers/concerns/authenticable_helper_spec.rb
@@ -64,6 +64,17 @@ describe RailsJwtAuth::AuthenticableHelper, type: :helper do
       end
     end
 
+    context 'when user is not found' do
+      it 'fail!' do
+        token = user.regenerate_auth_token
+        jwt = RailsJwtAuth::JwtManager.encode(auth_token: token)
+        helper.request.env['HTTP_AUTHORIZATION'] = "Bearer #{jwt}"
+
+        user.delete
+        expect { helper.authenticate! }.to raise_error(RailsJwtAuth::NotAuthorized)
+      end
+    end
+
     context 'when user is trackable' do
       it 'call update tracked fields' do
         user = Object.new
@@ -74,6 +85,91 @@ describe RailsJwtAuth::AuthenticableHelper, type: :helper do
 
         expect(user).to receive(:update_tracked_fields!).and_return(true)
         helper.authenticate!
+      end
+    end
+  end
+
+  describe '#authenticate' do
+    before :all do
+      RailsJwtAuth.model_name = ActiveRecordUser.to_s
+      RailsJwtAuth.simultaneous_sessions = 1
+    end
+
+    let(:user) { FactoryBot.create(:active_record_user) }
+
+    context 'when jwt is valid' do
+      it 'success!' do
+        token = user.regenerate_auth_token
+        jwt = RailsJwtAuth::JwtManager.encode(auth_token: token)
+        helper.request.env['HTTP_AUTHORIZATION'] = "Bearer #{jwt}"
+
+        helper.authenticate
+        expect(helper.current_user).to eq(user)
+      end
+    end
+
+    context 'when jwt is invalid' do
+      it 'fail!' do
+        allow(RailsJwtAuth::JwtManager)
+          .to receive(:decode_from_request).and_raise(JWT::DecodeError)
+
+        expect { helper.authenticate }.not_to raise_error
+        expect(helper.current_user).to eq(nil)
+      end
+    end
+
+    context 'when jwt is expired' do
+      it 'fail!' do
+        allow(RailsJwtAuth::JwtManager)
+          .to receive(:decode_from_request).and_raise(JWT::ExpiredSignature)
+
+        expect { helper.authenticate }.not_to raise_error
+        expect(helper.current_user).to eq(nil)
+      end
+    end
+
+    context 'when jwt verification fail' do
+      it 'fail!' do
+        allow(RailsJwtAuth::JwtManager)
+          .to receive(:decode_from_request).and_raise(JWT::VerificationError)
+
+        expect { helper.authenticate }.not_to raise_error
+        expect(helper.current_user).to eq(nil)
+      end
+    end
+
+    context 'when session_token is invalid' do
+      it 'fail!' do
+        allow(RailsJwtAuth::JwtManager)
+          .to receive(:decode_from_request).and_return([{auth_token: 'invalid'}])
+
+        expect { helper.authenticate }.not_to raise_error
+        expect(helper.current_user).to eq(nil)
+      end
+    end
+
+    context 'when user is not found' do
+      it 'fail!' do
+        token = user.regenerate_auth_token
+        jwt = RailsJwtAuth::JwtManager.encode(auth_token: token)
+        helper.request.env['HTTP_AUTHORIZATION'] = "Bearer #{jwt}"
+
+        user.delete
+        expect { helper.authenticate }.not_to raise_error
+        expect(helper.current_user).to eq(nil)
+      end
+    end
+
+    context 'when user is trackable' do
+      it 'call update tracked fields' do
+        user = Object.new
+        user.extend RailsJwtAuth::Trackable
+
+        allow(RailsJwtAuth::JwtManager).to receive(:decode_from_request).and_return([{}])
+        allow(RailsJwtAuth.model).to receive(:get_by_token).and_return(user)
+
+        expect(user).to receive(:update_tracked_fields!).and_return(true)
+        helper.authenticate
       end
     end
   end
