931bd51bf58fe8fa688f6f52cc0456c37e63dd02
rjurado <rjurado01@gmail.com>
2020-02-20 21:13:44 +0100

    Removed hardcoded email field from confirmation controller.

--- app/controllers/rails_jwt_auth/confirmations_controller.rb
+++ app/controllers/rails_jwt_auth/confirmations_controller.rb
@@ -4,7 +4,10 @@ module RailsJwtAuth
     include RenderHelper
 
     def create
-      user = RailsJwtAuth.model.where(email: confirmation_create_params[:email]).first
+      user = RailsJwtAuth.model.where(
+        email: confirmation_create_params[RailsJwtAuth.email_field_name]
+      ).first
+
       return render_422(email: [{error: :not_found}]) unless user
 
       user.send_confirmation_instructions ? render_204 : render_422(user.errors.details)
