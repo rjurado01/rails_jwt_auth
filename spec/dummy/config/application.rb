8e6f1ff8cd816cf3fe740e0d0f8f94b7c16acf18
rjurado01 <rjurado01@gmail.com>
2021-07-10 16:55:15 +0200

    Fixed rails warning
    Rails.application.config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -30,6 +30,8 @@ module Dummy
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_mailer.delivery_job = 'ActionMailer::MailDeliveryJob'
   end
 end
 
